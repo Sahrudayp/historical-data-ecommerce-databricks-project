ee07afed4abc1daebd0aa7508b11a40dc4b70ccb
Sahrudayp <151670100+Sahrudayp@users.noreply.github.com>
2026-03-25 20:13:45 +0530

    Add files via upload

---
+++ setup_catalog.py
@@ -0,0 +1,26 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC CREATE CATALOG IF NOT EXISTS ecommerce;
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC USE catalog ecommerce;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE SCHEMA IF NOT EXISTS ecommerce.bronze;
+# MAGIC CREATE SCHEMA IF NOT EXISTS ecommerce.silver;
+# MAGIC CREATE SCHEMA IF NOT EXISTS ecommerce.gold;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SHOW DATABASES FROM ecommerce;
+
+# COMMAND ----------
+
+# %sql
+# DROP CATALOG IF EXISTS ecommerce CASCADE;
